a6fd9f86976827ebc7044c1b367b435e50583f6f
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-12-05 15:27:17 +0100

    improve swagger API spec https://github.com/moby/moby/pull/35593

--- src/main/java/it/dockins/jocker/model/ContainerInspect.java
+++
@@ -1,9 +0,0 @@
-package it.dockins.jocker.model;
-
-import it.dockins.jocker.model.InlineResponse200;
-
-/**
- * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
- */
-public class ContainerInspect extends InlineResponse200 {
-}
