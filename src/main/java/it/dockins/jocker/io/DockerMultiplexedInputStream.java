afe3cac29bd940d517bc3a6730b8e176911e909f
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-10-06 11:57:17 +0200

    Apply fix contibuted to docker-plugin by @a-ba
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- src/main/java/it/dockins/jocker/io/DockerMultiplexedInputStream.java
+++ src/main/java/it/dockins/jocker/io/DockerMultiplexedInputStream.java
@@ -22,27 +22,28 @@ public class DockerMultiplexedInputStream extends InputStream {
 
     @Override
     public int read() throws IOException {
-        if (readInternal() <0) return -1;
+        readInternal();
         next--;
         return multiplexed.read();
     }
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-        if (readInternal() <0) return -1;
+        readInternal();
         int byteRead = multiplexed.read(b, off, Math.min(next, len));
         next -= byteRead;
         return byteRead;
     }
 
-    private int readInternal() throws IOException {
+    private void readInternal() throws IOException {
         while (next == 0) {
             byte[] header = new byte[8];
-            int i = multiplexed.read(header);
-            if (i < 0) return -1; // EOF
-
-            if (i != 8)
-                throw new IOException("Expected 8 bytes application/vnd.docker.raw-stream header, got " + i);
+            int todo = 8;
+            while (todo > 0) {
+                int i = multiplexed.read(header, 8-todo, todo);
+                if (i < 0) return; // EOF
+                todo -= i;
+            }
 
             int size = ((header[4] & 0xff) << 24) + ((header[5] & 0xff) << 16) + ((header[6] & 0xff) << 8) + (header[7] & 0xff);
             switch (header[0]) {
@@ -54,16 +55,16 @@ public class DockerMultiplexedInputStream extends InputStream {
                     byte[] payload = new byte[size];
                     int received = 0;
                     while (received < size) {
-                        i = multiplexed.read(payload, received, size - received);
+                        int i = multiplexed.read(payload, received, size - received);
+                        if (i < 0) break; // EOF
                         received += i;
                     }
-                    System.err.println(new String(payload, StandardCharsets.UTF_8));
+                    System.err.printf("Unexpected data on container stderr: {}\n", new String(payload, StandardCharsets.UTF_8));
                     break;
                 default:
                     throw new IOException("Unexpected application/vnd.docker.raw-stream frame type " + header[0]);
             }
         }
-        return next;
     }
 
     @Override
